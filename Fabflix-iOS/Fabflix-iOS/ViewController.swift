5ab44d8e15efa6d85b4ada9e2add4be4e09b3b75
Zidan Li <zidanli@Zidans-iMac.local>
2020-07-15 23:23:26 -0400

    init

---
+++ Fabflix-iOS/Fabflix-iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Fabflix-iOS
+//
+//  Created by Zidan Li on 7/15/20.
+//  Copyright © 2020 Haoming Gao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
